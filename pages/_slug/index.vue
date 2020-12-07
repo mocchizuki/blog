77cd9ea289ef35ed7884967cf3ac43a08e7d8016
Yuhei <yuhei@mochi.local>
2020-12-08 01:40:30 +0900

    firstcommit

---
+++ pages/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://mczk.microcms.io/api/v1/blog/${params.slug}`,
+      {
+        headers: { 'X-API-KEY': 'd8d3d5dd-f732-48da-bb32-29ebf7de8220' }
+      }
+    )
+    return data
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://mczk.microcms.io/api/v1/blog',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-API-KEY': 'd8d3d5dd-f732-48da-bb32-29ebf7de8220' }
+      }
+    )
+    return data
+  }
+}
+</script>
